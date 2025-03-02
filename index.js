c0085c8164270ad01c41fb8ab19daa01bc261dd6
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-03-02 23:35:01 +0530

    Update index.js
    animation and custom score popup message is added

--- index.js
+++ index.js
@@ -86,8 +86,76 @@ function showResult() {
     });
     resultContainer.innerHTML = `<h3>Your Score: ${score} / ${questions.length}</h3>`;
     resultContainer.style.display = 'block';
+    const myPopup = new Popup({
+        id: "my-popup",
+        title: "Your Score : ",
+        content: `
+            ${score} / ${questions.length} 
+            `
+    });
+    myPopup.show();
+    
+    // Call fireCracker() three times with 500ms interval
+    setTimeout(fireCracker, 0);     // First time immediately
+    setTimeout(fireCracker, 500);   // Second time after 500ms
+    setTimeout(fireCracker, 1000);  // Third time after 1000ms (1s)
+
 }
 
 submitBtn.addEventListener('click', showResult);
 
 loadQuiz();
+
+
+
+
+
+
+
+
+
+    
+function fireCracker(){
+        
+    const count = 200,
+    defaults = {
+      origin: { y: 0.7 },
+    };
+  
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+  
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  
+  fire(0.2, {
+    spread: 60,
+  });
+  
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+
+    }
